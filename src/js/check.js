780db8a10d07ba5a37984130545b56e0a69145f3
Vesper <vesperlynd309@yandex.ru>
2016-10-06 19:47:20 +0300

    Поправила стиль

--- src/js/check.js
+++ src/js/check.js
@@ -1,10 +1,12 @@
 "use strict";
+
 var a; 
 var b;
+
 function getMessage(a, b) {
    if (typeof a == "boolean") {
       if (a == true) {
-         return ("Переданное GIF-изображение анимировано и содержит "+ b + " кадров");
+         return ("Переданное GIF-изображение анимировано и содержит " + b + " кадров");
       } else {
          return ("Переданное GIF-изображение не анимировано");
       }
