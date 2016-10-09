57d3b3b601d468481621b865872cbd3df50ad886
Vesper <vesperlynd309@yandex.ru>
2016-10-09 13:06:05 +0300

    Исправление ошибок в check.js

--- src/js/check.js
+++ src/js/check.js
@@ -3,14 +3,14 @@
 function getMessage(a, b) {
   if (typeof a === 'boolean') {
     if (a === true) {
-       return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
+      return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
      } else {
-       return ('Переданное GIF-изображение не анимировано');
-     }
+     return ('Переданное GIF-изображение не анимировано');
+    }
 
   } else if (typeof a === 'number') {
-    return ('Переданное SVG-изображение содержит ' + a +  'объектов и ' + (b * 4) + ' атрибутов');
-   } else if (typeof a === 'object') {
+    return ('Переданное SVG-изображение содержит ' + a + 'объектов и ' + (b * 4) + ' атрибутов');
+  } else if (typeof a === 'object') {
      var amountOfRedPoints = 0;
      for ( var i = 0; i < a.length; i++) {
        amountOfRedPoints += a[i];
@@ -23,4 +23,5 @@ function getMessage(a, b) {
      } return ('Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей');
    } else {
      return ('Переданы некорректные данные');
-   }}
+   }
+} ();
