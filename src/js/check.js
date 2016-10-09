006052208f0ff955c0ff01892a158a5e26898c58
Vesper <vesperlynd309@yandex.ru>
2016-10-09 16:53:04 +0300

    Исправление ошибок в check.js

--- src/js/check.js
+++ src/js/check.js
@@ -4,18 +4,18 @@ function getMessage(a, b) {
   if (typeof a === 'boolean') {
     if (a === true) {
       return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
-    } else {
-    return ('Переданное GIF-изображение не анимировано');
+   } else {
+   return ('Переданное GIF-изображение не анимировано');
     }
 
   } else if (typeof a === 'number') {
     return ('Переданное SVG-изображение содержит ' + a + 'объектов и ' + (b * 4) + ' атрибутов');
   } else if (typeof a === 'object') {
-    var amountOfRedPoints = 0;
-    for ( var i = 0; i < a.length; i++) {
+   var amountOfRedPoints = 0;
+   for ( var i = 0; i < a.length; i++) {
        amountOfRedPoints += a[i];
      } return ('Количество красных точек во всех строчках изображения: ' + amountOfRedPoints);
-  } else if (typeof a === 'object' && typeof b === 'object') {
+ } else if (typeof a === 'object' && typeof b === 'object') {
      var artifactsSquare = 0;
      var j;
      for ( i = 0, j = 0; i < a.length && j < b.length; i++, j++) {
