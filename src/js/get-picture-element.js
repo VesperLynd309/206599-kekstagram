444bc72564c83a167a747a4fdf10aa5477713db4
Vesper <vesperlynd309@yandex.ru>
2016-10-26 21:28:31 +0300

    CommonJS

--- src/js/get-picture-element.js
+++ src/js/get-picture-element.js
@@ -1,52 +1,51 @@
 'use strict';
 
 // Создание DOM-элемента картинки и загрузка фото
-define(function() {
-  return function(picture) {
-    document.querySelector('.filters').classList.add('hidden');
+module.exports = function(picture) {
+  document.querySelector('.filters').classList.add('hidden');
 
-    var PICTURE_LOAD_TIMEOUT = 1000;
-    var template = document.querySelector('#picture-template');
-    var templateContainer = 'content' in template ? template.content : template;
-    var pictureElement = templateContainer.querySelector('.picture').cloneNode(true);
+  var PICTURE_LOAD_TIMEOUT = 1000;
+  var template = document.querySelector('#picture-template');
+  var templateContainer = 'content' in template ? template.content : template;
+  var pictureElement = templateContainer.querySelector('.picture').cloneNode(true);
 
-    pictureElement.querySelector('.picture-likes').textContent = picture.likes;
-    pictureElement.querySelector('.picture-comments').textContent = picture.comments;
+  pictureElement.querySelector('.picture-likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture-comments').textContent = picture.comments;
 
-    var pictureImage = new Image(182, 182);
-    var pictureImageTimeout = null;
+  var pictureImage = new Image(182, 182);
+  var pictureImageTimeout = null;
 
-    //Проверка наличия свойства preview
-    function emptyObject(picture) {
-      if (picture.preview) {
-      return true;
-    }
-      return false;
-    }
+  //Проверка наличия свойства preview
+  function emptyObject(picture) {
+    if (picture.preview) {
+    return true;
+  }
+    return false;
+  }
 
-    pictureImage.onload = function() {
-      clearTimeout(pictureImageTimeout);
-      pictureElement.querySelector('img').src = picture.url;
-    };
-
-    pictureImage.onerror = function() {
-      if (emptyObject(picture) === true) {
-        clearTimeout(pictureImageTimeout);
-        pictureElement.querySelector('img').src = picture.preview;
-      } else {
-
-        pictureElement.classList.add('picture-load-failure');
-      }
-    };
-
-    pictureImage.src = picture.url;
-
-    pictureImageTimeout = setTimeout(function() {
-      pictureElement.classList.add('picture-load-failure');
-    }, PICTURE_LOAD_TIMEOUT);
-
-    return pictureElement;
-
-    document.querySelector('.filters').classList.remove('hidden');
+  pictureImage.onload = function() {
+    clearTimeout(pictureImageTimeout);
+    pictureElement.querySelector('img').src = picture.url;
   };
-});
+
+  pictureImage.onerror = function() {
+    if (emptyObject(picture) === true) {
+      clearTimeout(pictureImageTimeout);
+      pictureElement.querySelector('img').src = picture.preview;
+    } else {
+
+      pictureElement.classList.add('picture-load-failure');
+    }
+  };
+
+  pictureImage.src = picture.url;
+
+  pictureImageTimeout = setTimeout(function() {
+    pictureElement.classList.add('picture-load-failure');
+  }, PICTURE_LOAD_TIMEOUT);
+
+  return pictureElement;
+
+  document.querySelector('.filters').classList.remove('hidden');
+};
+
