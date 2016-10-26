1d8a173991578e4c2c97713c4708b501a743c1cf
Vesper <vesperlynd309@yandex.ru>
2016-10-26 21:49:35 +0300

    CommonJS

--- src/js/load.js
+++ src/js/load.js
@@ -1,17 +1,16 @@
 'use strict';
 
 //JSONP
-module.exports =  function(url, callback, callbackName) {
-	if (!callbackName) {
+module.exports = function(url, callback, callbackName) {
+  if (!callbackName) {
     callbackName = 'cb' + Date.now();
-  }
+  };
 
   window[callbackName] = function(pictures) {
     callback(pictures);
-  }
+  };
 
   var script = document.createElement('script');
   script.src = url + '?callback=' + callbackName;
   document.body.appendChild(script);
 };
-
