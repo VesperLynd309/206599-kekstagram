16146f81d345c43fa403690bd7025c0f94c46981
Vesper <vesperlynd309@yandex.ru>
2016-10-26 22:03:13 +0300

    CommonJS

--- src/js/data.js
+++ src/js/data.js
@@ -2,6 +2,8 @@
 
 //Получение данных со списком картинок
 (function() {
+  document.querySelector('.filters').classList.add('hidden');
+
   var load = require('../js/load');
   var getPictureElement = require('../js/get-picture-element');
 
@@ -11,9 +13,10 @@
   // Отрисовка списка на странице
   var renderPictures = function(pictures) {
  	pictures.forEach(function(picture) {
-	container.appendChild(getPictureElement(picture));
+       container.appendChild(getPictureElement(picture));
 	});
   };
 
   load(PICTURES_LOAD_URL, renderPictures, '__jsonpCallback');
+   document.querySelector('.filters').classList.remove('hidden');
 })();
