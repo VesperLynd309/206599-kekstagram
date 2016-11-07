91f325d6844789aa053922bb0071b4d228e26fda
Vesper <vesperlynd309@yandex.ru>
2016-11-07 14:31:09 +0300

    onerror для галереи

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -8,18 +8,18 @@ var Gallery = function() {
   this.galleryClose = document.querySelector('.gallery-overlay-close');
   this.galleryPicture = document.querySelector('.gallery-overlay-image');
 };
-
+ 
 Gallery.prototype.setPictures = function(pictures) {
   this.pictures = pictures;
 };
-
+ 
 Gallery.prototype.show = function(number) {
   var self = this;
-
+ 
   this.galleryClose.onclick = function() {
     self.hide();
   };
-
+ 
   this.galleryPicture.onclick = function() {
     if (number === self.pictures.length - 1) {
       self.setActivePicture(0);
@@ -27,38 +27,30 @@ Gallery.prototype.show = function(number) {
       self.setActivePicture(number + 1);
     }
   };
-
+ 
   this.galleryContainer.classList.remove('invisible');
-
-  self.setActivePicture(number);
+ 
+  this.setActivePicture(number);
 };
-
+ 
 Gallery.prototype.hide = function() {
   this.galleryContainer.classList.add('invisible');
   this.galleryClose.onclick = null;
   this.galleryPicture.onclick = null;
 };
-
+ 
 Gallery.prototype.setActivePicture = function(number) {
   this.pictureLikes = document.querySelector('.likes-count');
   this.pictureComments = document.querySelector('.comments-count');
-  self.activePicture = number;
-  this.galleryPictureContainer = document.querySelector('.gallery-overlay-preview');
+  this.activePicture = number;
 
-  this.galleryPictureContainer = new Image();
-  var that = this;
-
-  this.galleryPictureContainer.onload = function(evt) {
-  	that.galleryPicture.src = that.pictures[number].url;
+  this.galleryPicture.onerror = function() {
+    self.galleryPicture.classList.add('picture-load-failure');
   };
-
-  this.galleryPictureContainer.onerror = function(evt) {
-  	that.galleryPicture.classList.add('picture-load-failure');
-  };
-
-  this.galleryPictureContainer = this.pictures[number].url;
+  
+  this.galleryPicture.src = this.pictures[number].url;
   this.pictureLikes.textContent = this.pictures[number].likes;
   this.pictureComments.textContent = this.pictures[number].comments;
 };
-
+ 
 module.exports = new Gallery();
